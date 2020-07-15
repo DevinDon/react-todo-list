28d8e80a6c9a1d43e81f875610294e25aea3352f
Devin Don <I.INF@Outlook.com>
2020-07-15 17:25:26 +0800

    feat: Item 样式

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import { CheckCircleOutlined, CloseCircleOutlined, LoadingOutlined } from '@ant-design/icons';
+import React from 'react';
+import styles from './Item.module.css';
 
-export function Item() {
+export function Item({ title, completed, time }) {
   return (
-    <div>Item Component</div>
+    <div className={styles.item}>
+      <span className={styles.left}>
+        {completed ? <CheckCircleOutlined className={styles.icon} /> : <LoadingOutlined className={styles.icon} />}
+        <span>{title}</span>
+      </span>
+      <CloseCircleOutlined className={styles.delete} />
+    </div>
   );
 }
