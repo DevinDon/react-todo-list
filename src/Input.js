7de915e57e636cbcbcb8e390da74eb674cab17e6
Devin Don <I.INF@Outlook.com>
2020-07-15 17:49:05 +0800

    feat: input 功能完成

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import styles from './Input.module.css';
 
 export function Input({ addTodo }) {
+  /**
+   * 检测回车，添加新待办事件，并清空输入框
+   * 
+   * @param {Event} event input 按键事件
+   */
+  const handleEnter = event => {
+    if (event.keyCode !== 13) { return; }
+    addTodo(event.currentTarget.value);
+    event.currentTarget.value = '';
+  };
   return (
-    <input class="input" type="text" placeholder="新增一条待办事项..." />
+    <input
+      className="input"
+      type="text"
+      placeholder="新增一条待办事项..."
+      onKeyDown={handleEnter}
+    />
   );
 }
