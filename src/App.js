271133c27e8d4dad0182888ccd4540c7bc1de744
Devin Don <I.INF@Outlook.com>
2020-07-15 17:07:53 +0800

    feat: 启用样式

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Input } from './Input'
+import { Item } from './Item'
+import styles from './App.module.css';
 
-export function App() {
-  return (
-    <div>Hello, world!</div>
-  );
+// interface Todo {
+//   title: string;
+//   completed: boolean;
+//   time: Date | number; // 为了便于存储
+// }
+
+function loadTodosFromLocal() {
+  const todos = localStorage.getItem('todos');
+  return todos && JSON.parse(todos);
 }
 
+function saveTodosToLocal(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+export function App() {
+  const [todos, setTodos] = useState(
+    loadTodosFromLocal() || [
+      { title: '尝试新增一件待办事项', complete: false, time: Date.now() },
+      { title: '已完成的事项', complete: true, time: Date.now() + 1 },
+    ]
+  );
+  // useEffect(() => {
+
+  // });
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <Input />
+        <div className="list">
+          {todos.map(todo => <Item key={todo.time} {...todo}></Item>)}
+        </div>
+      </div>
+    </div>
+  );
+}
