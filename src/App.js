90368e65f86efc82a841c5414fb22319b025304e
Devin Don <I.INF@Outlook.com>
2020-07-15 18:01:51 +0800

    feat: 实现 Item 组件的删除与完成待办功能

--- src/App.js
+++ src/App.js
@@ -21,19 +21,25 @@ function saveTodosToLocal(todos) {
 export function App() {
   const [todos, setTodos] = useState(
     loadTodosFromLocal() || [
-      { title: '尝试新增一件待办事项', complete: false, time: Date.now() },
-      { title: '已完成的事项', complete: true, time: Date.now() + 1 },
+      { title: '尝试新增一件待办事项', completed: false, time: Date.now() },
+      { title: '已完成的事项', completed: true, time: Date.now() + 1 },
     ]
   );
-  // useEffect(() => {
-
-  // });
+  const addTodo = title => setTodos([{ title, completed: false, time: Date.now() }, ...todos]);
+  const completeTodo = time => setTodos(
+    todos.map(todo => {
+      todo.time === time && (todo.completed = !todo.completed);
+      return todo;
+    })
+  );
+  const removeTodo = time => setTodos(todos.filter(todo => todo.time !== time));
+  useEffect(() => saveTodosToLocal(todos), [todos]);
   return (
     <div className={styles.page}>
       <div className={styles.container}>
-        <Input />
+        <Input addTodo={addTodo} />
         <div className={styles.list}>
-          {todos.map(todo => <Item key={todo.time} {...todo}></Item>)}
+          {todos.map(todo => <Item key={todo.time} completeTodo={completeTodo} removeTodo={removeTodo} {...todo}></Item>)}
         </div>
       </div>
     </div>
--- src/Item.js
+++ src/Item.js
@@ -2,14 +2,16 @@ import { CheckCircleOutlined, CloseCircleOutlined, LoadingOutlined } from '@ant-
 import React from 'react';
 import styles from './Item.module.css';
 
-export function Item({ title, completed, time }) {
+export function Item({ title, completed, time, completeTodo, removeTodo }) {
   return (
     <div className={styles.item}>
       <span className={styles.left}>
-        {completed ? <CheckCircleOutlined className={styles.icon} /> : <LoadingOutlined className={styles.icon} />}
-        <span>{title}</span>
+        <div className={styles.icon} onClick={() => completeTodo(time)}>
+          {completed ? <CheckCircleOutlined /> : <LoadingOutlined />}
+        </div>
+        <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{title}</span>
       </span>
-      <CloseCircleOutlined className={styles.delete} />
+      <CloseCircleOutlined className={styles.delete} onClick={() => removeTodo(time)} />
     </div>
   );
 }
