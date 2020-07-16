9494d10d4a6991a4bc4338b55b6b4329bf9b5a81
Devin Don <I.INF@Outlook.com>
2020-07-16 09:29:54 +0800

    build: 2020-07-16 09:29

---
+++ build/precache-manifest.30821a9ba6d46297daaea4742ba339be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52f3206883511fee95403104045a023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ade61ff353618aee2ad",
+    "url": "/static/css/main.03c7cda1.chunk.css"
+  },
+  {
+    "revision": "a26ac2dd5ba4627e06d3",
+    "url": "/static/js/2.aa517720.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aa517720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ade61ff353618aee2ad",
+    "url": "/static/js/main.a0ce4482.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
