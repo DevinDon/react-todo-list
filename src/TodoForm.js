a14db4d1fbcb560aec0bd32062e7bc07a943b194
Devin Don <I.INF@Outlook.com>
2020-07-26 19:14:45 +0800

    feat: search function

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -16,10 +16,13 @@ export function TodoForm({ addTodo, todos, setFilteredTodos }) {
    * @param {string} title 待办事项的标题
    */
   const filter = title => {
+    // debug output
     console.log('filter', title);
+    // 筛选包含输入框内容的待办事项
     const newTodos = todos.filter(todo => todo.title.includes(title));
+    // 更新展示列表
     setFilteredTodos(newTodos);
-  }
+  };
 
   const handleChange = event => {
     const current = event.target.value;
